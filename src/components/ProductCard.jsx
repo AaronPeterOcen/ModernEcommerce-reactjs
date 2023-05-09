6e8b3f59a1ef2d677fa820bf14d4656363562be3
Aaronpteer <ocenaaronpeter2001@gmail.com>
2023-05-09 11:11:05 +0300

    finished witht the ui of the homepage

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,22 +1,72 @@
 import React from "react";
+import { DynamicStar } from "react-dynamic-star";
+import Rater from "react-rater";
+
+import "react-rater/lib/react-rater.css";
+import Rating from "react-rating";
+import { Link } from "react-router-dom";
 
 const ProductCard = () => {
   return (
     <div className="col-3">
-      <div className="product-card">
+      <Link className="product-card position-relative">
+        <div className="wishlist-icon position-absolute">
+          <Link>
+            <img src="images/wish.svg" alt="wishlist" />
+          </Link>
+        </div>
         <div className="product-image">
-          <img src="images/watch.jpg" alt="product image" />
+          <img
+            src="images/watch.jpg"
+            className="img-fluid"
+            alt="product image"
+          />
+          <img
+            src="images/watch-1.avif"
+            className="img-fluid"
+            alt="product image"
+          />
         </div>
         <div className="product-details">
           <h6 className="brand">Havels</h6>
           <h5 className="product-title">
             Kids headphones 10 pack multi colored for students
           </h5>
+
+          <Rater total={5} rating={3} size={20} />
+
           <p className="price">$100</p>
         </div>
-      </div>
+        <div className="action-bar position-absolute">
+          <div className="d-flex flex-column gap-15">
+            <Link>
+              <img src="images/prodcompare.svg" alt="addcart" />
+            </Link>
+            <Link>
+              <img src="images/view.svg" alt="viewcart" />
+            </Link>
+            <Link>
+              <img src="images/add-cart.svg" alt="addcart" />
+            </Link>
+          </div>
+        </div>
+      </Link>
     </div>
   );
 };
 
 export default ProductCard;
+
+{
+  /* <Rating
+            stop={5}
+            start={0}
+            initialRating={3}
+            emptySymbol="fa fa-star-o"
+            fullSymbol="fa fa-star"
+            readonly
+            /> */
+}
+{
+  /* <DynamicStar rating={3} totalStars={5} width={30} height={30} /> */
+}
