71c36717373f36345348c00464c16686acc1c7c7
Aaronpteer <ocenaaronpeter2001@gmail.com>
2023-05-11 13:05:05 +0300

    layout of the blogs page

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -3,21 +3,19 @@ import { Link } from "react-router-dom";
 
 const BlogCard = () => {
   return (
-    <div className="col-3">
-      <div className="blog-card">
-        <div className="card-image">
-          <img src="images/blog-1.jpg" alt="blog" className="img-fluid" />
-        </div>
-        <div className="blog-content">
-          <p className="date">3 May, 2023</p>
-          <h5 className="title">A beautiful Wednesday Morning Renaissance</h5>
-          <p className="desc">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam.
-          </p>
-          <Link className="button" to="">
-            READ MORE
-          </Link>
-        </div>
+    <div className="blog-card">
+      <div className="card-image">
+        <img src="images/blog-1.jpg" alt="blog" className="img-fluid w-100" />
+      </div>
+      <div className="blog-content">
+        <p className="date">3 May, 2023</p>
+        <h5 className="title">A beautiful Wednesday Morning Renaissance</h5>
+        <p className="desc">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam.
+        </p>
+        <Link className="button" to="">
+          READ MORE
+        </Link>
       </div>
     </div>
   );
